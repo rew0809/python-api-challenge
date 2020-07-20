9c1d90ea88174589525e2d7ab26d1e32b38e2a7f
REW <rewelling09@gmail.com>
2020-07-20 11:12:08 -0500

    Add Final Code

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "e9778d85c98f69b9c019b0848e68cf1e"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCDc0oRKkPdXBm6McMx-e_BPrhoD3awbX8"
