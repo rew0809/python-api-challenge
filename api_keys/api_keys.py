a4e828e490d381cb799d5270ff97f86cdbfbf928
REW <rewelling09@gmail.com>
2020-07-20 11:14:23 -0500

    Update Api Key Folder

--- api_keys/api_keys.py
+++ api_keys/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "e9778d85c98f69b9c019b0848e68cf1e"
+weather_api_key = "Your-API-Key-Here"
 
 # Google API Key
-g_key = "AIzaSyCDc0oRKkPdXBm6McMx-e_BPrhoD3awbX8"
+g_key = "Your-API-Key-Here"
